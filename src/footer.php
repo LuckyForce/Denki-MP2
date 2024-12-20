e60960a0ca0017e9987e13efdf1236f30c93d6c4
Adrian Schauer <adrian.schauer@aon.at>
2024-12-20 19:03:37 +0100

    footer

---
+++ src/footer.php
@@ -0,0 +1,29 @@
+<?php
+//Footer Daten
+$name = "Adrian Schauer und Ilie Borsanov";
+$address = "TU Wien";
+$email = "info@adrian-schauer.at";
+?>
+</div>
+<div class="bg-dark-grey purple mb-0">
+    <div class="row m-0">
+        <div class="col centralizer">
+            <br>
+            <br>
+            <p>Impressum:</p>
+            <p><?= $name ?></p>
+            <p><?= $address ?></p>
+            <p>
+                <a href="mailto:<?= $email ?>" class="mail purple"><?= $email ?></a>
+            </p>
+            <br>
+        </div>
+        <div class="col centralizer">
+            <p>&copy; 2024 TU Wien<br>MP 2</p>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
